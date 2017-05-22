49ad1fb98f49bd4c671184dc1787cebeb4d7c4b6
Dylan Chong <dylan.chong@icloud.com>
2017-05-22 21:20:43 +1200

    Revert grammars back to aenea ones

--- scripts/copy-scripts.py
+++ scripts/copy-scripts.py
@@ -1,11 +1,8 @@
 import shutil
 import os
-from glob import glob
 
 # Ensure working directory is the directory containing ".git"
 if os.getcwd().endswith("/scripts"):
     os.chdir("..")
 
-grammar_files = glob("./aenea-grammars/*")
-for file in grammar_files:
-    shutil.copy(file, "./MacroSystem/")
+shutil.copyfile("./aenea-grammars/_vim/_vim.py", "./MacroSystem/_vim.py")
