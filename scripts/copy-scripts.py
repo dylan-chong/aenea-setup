1f27cba2641ff1b4a20158368176a5deed62b25d
Dylan Chong <dylan.chong@icloud.com>
2017-07-03 17:33:14 +1200

    Add logs to copy-scripts.py

--- scripts/copy-scripts.py
+++ scripts/copy-scripts.py
@@ -8,7 +8,7 @@ if os.getcwd().endswith('/scripts'):
 # Files and directories to copy into a new place. The destination is deleted
 files_to_copy = [
     # (source file/directory name, source directory, destination directory)
-    ('_vim.py', './aenea-grammars/_vim/', './MacroSystem/'),
+    ('_charwise_vim.py', './aenea-grammars/_charwise_vim/', './MacroSystem/'),
     ('aenea/', './aenea/client/', './MacroSystem/')
     ]
 
@@ -33,6 +33,7 @@ def ensure_path_doesnt_exist(path):
     if not os.path.exists(path):
         return
 
+    print 'Removing existing file/directory ' + path
     if os.path.isdir(path):
         shutil.rmtree(path)
     else:
@@ -46,6 +47,7 @@ def copy_files_and_dirs(file_tuples):
 
         ensure_path_doesnt_exist(destination)
 
+        print 'Copying {} to {}'.format(source, destination)
         if os.path.isdir(source):
             shutil.copytree(source, destination)
         else:
